7430e0d1667d3305433dac5df66f0fed013f6f5d
lsm <seungmin941215@awasoft.co.kr>
2022-04-26 16:12:24 +0900

    React 18 권고사항_render -> createRoot

--- slidingmenu/src/index.js
+++ slidingmenu/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import MenuContainer from './MenuContainer'
 
-ReactDOM.render(
-    <MenuContainer/>,
-    document.querySelector('#container')
+// ReactDOM.render(
+//     <MenuContainer/>,
+//     document.querySelector('#container')
+// )
+
+// React 18에서는
+const root = document.getElementById('container')
+
+ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+        <MenuContainer/>
+    </React.StrictMode>
 )
