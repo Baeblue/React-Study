707680baeb2e578b2a434733d575296551689c16
lsm <seungmin941215@awasoft.co.kr>
2022-04-26 15:31:35 +0900

    SlidingMenu_슬라이딩 작업

--- slidingmenu/src/MenuContainer.js
+++ slidingmenu/src/MenuContainer.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
+import MenuButton from './MenuButton'
+import Menu from './Menu' 
 
 class MenuContainer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            visible: false
+        }
+
+        this.handleMouseDown = this.handleMouseDown.bind(this)
+        this.toggleMenu = this.toggleMenu.bind(this)
+    }
+
+    handleMouseDown(e) {
+        this.toggleMenu()
+
+        console.log('clicked')
+        e.stopPropagation() // 이벤트가 상위 엘리먼트에 전달되지 않도록 막아준다.
+    }
+
+    toggleMenu() {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
+
     render() {
         return (
             <div>
+                <MenuButton handleMouseDown={this.handleMouseDown}/>
+                <Menu handleMouseDown={this.handleMouseDown}
+                      menuVisibility={this.state.visible}/>
                 <div>
                     <p>Can you spot the item that doesn't belong?</p>
                     <ul>
