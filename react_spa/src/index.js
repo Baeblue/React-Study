f57a4664e8297edc7589c3bcff473cbf0799804b
lsm <seungmin941215@awasoft.co.kr>
2022-04-27 09:29:40 +0900

    React_SPA_react-router-dom 설치 및 초기 프레임 보여주기

---
+++ react_spa/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Main from './Main'
+
+var root = document.getElementById('root')
+
+ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+        <Main/>
+    </React.StrictMode>
+)
